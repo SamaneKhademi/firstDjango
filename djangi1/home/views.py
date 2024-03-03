f7f32c9fdaff5e209d4408fc7cb3a2874a5b704c
Samane Khademi <s.khademi1992@gmail.com>
2024-03-04 02:56:23 +0330

    url, def index. for home page

--- djangi1/home/views.py
+++ djangi1/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request=request, template_name='index.html', context={})
 
 # Create your views here.
