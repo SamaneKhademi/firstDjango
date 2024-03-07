2d90ef16e2673e00cfc90475615ad76b25f29abf
Samane Khademi <s.khademi1992@gmail.com>
2024-03-08 01:00:13 +0330

    set new template. nav float right

--- django1/home/views.py
+++ django1/home/views.py
@@ -10,7 +10,14 @@ def index(request):
 
 def signUp(request):
     page1=loader.get_template('signUp.html')
+    context={}
+    return HttpResponse(page1.render())
+
+
+
+    """
     user1=models.user(name="samane", email="s.khademi1992@gmail.com", password="123456")
     user1.save()
     context={"user":user1}
     return HttpResponse(page1.render(context))
+"""
