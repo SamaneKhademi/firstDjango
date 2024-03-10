773daf9ad1ca6cb5aad97351a1b7001aae433c21
Samane Khademi <s.khademi1992@gmail.com>
2024-03-10 15:05:56 +0330

    bootLink

--- django1/home/form.py
+++ django1/home/form.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class User(forms.Form):
-    Name = forms.CharField(required=True, widget=forms.TextInput())
-    Email = forms.EmailField(widget=forms.EmailInput())
-    Password = forms.CharField(widget=forms.PasswordInput)
+class Userform(forms.Form):
+    Name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control'}))
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    Password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- django1/home/views.py
+++ django1/home/views.py
@@ -10,18 +10,20 @@ def index(request):
 
 
 def signUp(request):
-    form1 = form.User()
-    return render(request=request, template_name='formsignUp.html',context={'form':form1})
+    form1 = form.Userform()
+    return render(request=request, template_name='formsignUp.html',context={'form': form1})
+
 
 def datatabel(request):
     alldata=models.User.objects.all()
-    return render(request=request, template_name='datatabel.html', context={"alldata":alldata})
+    return render(request=request, template_name='datatabel.html', context={"alldata": alldata})
+
 
 def saveData(request):
     if request.method == 'POST':
-        form2 = form.User(request.POST)
+        form2 = form.Userform(request.POST)
         if form2.is_valid():
             data = models.User(Email=form2.data['Email'], Password=form2.data['Password'])
             data.save()
             alldata = models.User.objects.all()
-            return render(request=request, template_name='datatabel.html', context={"alldata":alldata})
+            return render(request=request, template_name='datatabel.html', context={"alldata": alldata})
